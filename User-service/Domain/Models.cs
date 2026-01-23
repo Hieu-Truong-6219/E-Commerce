cf72f06eb63045c076e8364fef5208452a0b1e05
Hieu Truong <htruong6219@gmail.com>
2026-01-23 01:31:47 -0800

    Adding Email to UserInfo

--- User-service/Domain/Models.cs
+++ User-service/Domain/Models.cs
@@ -3,6 +3,7 @@ namespace UserMicroService.Domain;
 public class UserInfo
 {
     public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
 }
